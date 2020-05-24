6b4c1b7b6ed6ce1cebc819babf5939a161d8b637
fajar13k <fajar.fj13@gmail.com>
2020-05-25 03:55:41 +0700

    Push to GitHub Repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project Info
+
+Part of Andre Madarang's Programming Course: [Laravel Movie App](https://www.youtube.com/playlist?list=PLEhEHUEU3x5pYTjZze3fhYMB4Nl_WOHI4)
+
+## Used
+
+- Visual Studio Code (IDE)
+- Laravel
+- TMDB API
+- tailwindcss, purgecss
---
+++ resources/views/livewire/search-dropdown.blade.php
@@ -0,0 +1,55 @@
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
+	<input
+		wire:model.debounce.500ms="search"
+		type="text"
+		class="bg-gray-800 rounded-full w-64 px-4 py-1 focus:outline-none focus:shadow-outline"
+		placeholder="Search"
+		x-ref="search"
+		@keydown.window="
+			if (event.keyCode === 191) {
+				event.preventDefault();
+				$refs.search.focus();
+			}
+		"
+		@focus="isOpen = true"
+		@keydown="isOpen = true"
+		@keydown.escape.window="isOpen = false"
+		@keydown.shift.tab="isOpen = false"
+	/>
+
+	<div wire:loading class="spinner top-0 right-0 mr-4 mt-4"></div>
+
+	@if (strlen($search) >= 2)
+		<div
+			class="z-50 absolute bg-gray-800 text-sm rounded w-64 mt-4"
+			x-show.transition.opacity="isOpen"
+		>
+			@if ($searchResults->count() > 0)
+				<ul>
+					@foreach ($searchResults as $result)
+						<li class="border-b border-gray-700">
+							<a
+								href="{{ route('movies.show', $result['id']) }}"
+								class="block hover:bg-gray-700 px-3 py-3 flex items-center"
+								@if ($loop->last) @keydown.tab="isOpen = false" @endif
+							>
+								@if ($result['poster_path'])
+									<img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" class="w-16" />
+								@else
+									<img src="https://via.placeholder.com/50x75" alt="poster" class="w-16" />
+								@endif
+								@if ($result['release_date'])
+									<span class="ml-4">{{ $result['title'] . ' (' . \Carbon\Carbon::parse($result['release_date'])->format('Y') . ')' }}</span>
+								@else
+									<span class="ml-4">{{ $result['title'] . ' (Unknown Year)' }}</span>
+								@endif
+							</a>
+						</li>
+					@endforeach
+				</ul>
+			@else
+				<div class="px-3 py-3">No results for "{{ $search }}"</div>
+			@endif
+		</div>
+	@endif
+</div>
